452ee3e57967bd361361352447ff884a9f3d406f
root <root@RCRUZ-LAP.advanpro.com.mx>
2025-02-23 23:36:49 -0600

    Deploy 5

--- u3_tarea5/setup.sh
+++ u3_tarea5/setup.sh
@@ -1,34 +1,14 @@
 #!/bin/bash
 
-# Verificar e instalar dependencias si no están instaladas
-install_if_missing() {
-    if ! command -v "$1" &> /dev/null; then
-        echo "Instalando $1..."
-        sudo apt-get install -y "$1"
-    else
-        echo "$1 ya está instalado."
-    fi
-}
+# Actualizar el sistema e instalar dependencias
+apt-get update && apt-get install -y make wget
 
-# Actualizar la lista de paquetes
-sudo apt-get update
+## Bajar la última versión de 
+wget https://github.com/gohugoio/hugo/releases/download/v0.143.1/hugo_0.143.1_linux-amd64.deb
+dpkg -i hugo_0.143.1_linux-amd64.deb
 
-# Instalar make y wget si no están instalados
-install_if_missing make
-install_if_missing wget
-install_if_missing zip
+# Ejecutar el comando make build para reproducir el error
+make build
 
-# Descargar e instalar Hugo si no está instalado
-if ! command -v hugo &> /dev/null; then
-    echo "Descargando e instalando Hugo..."
-    wget https://github.com/gohugoio/hugo/releases/download/v0.143.1/hugo_0.143.1_linux-amd64.deb
-    sudo dpkg -i hugo_0.143.1_linux-amd64.deb || {
-        echo "Instalando dependencias faltantes para Hugo..."
-        sudo apt-get install -f -y
-    }
-    rm hugo_0.143.1_linux-amd64.deb # Limpiar el archivo descargado
-else
-    echo "Hugo ya está instalado."
-fi
 
-echo "Todas las herramientas necesarias están instaladas."
+
